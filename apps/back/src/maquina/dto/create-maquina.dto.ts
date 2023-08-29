d85cbd8744a3d313f221aacb0324e3412c4c204c
Lucas Mella <ra-20072665-2@alunos.unicesumar.edu.br>
2023-08-28 22:29:24 -0300

    Fiz partes básicas do crud, como definiçao inicial do schema

--- apps/back/src/maquina/dto/create-maquina.dto.ts
+++ apps/back/src/maquina/dto/create-maquina.dto.ts
@@ -1,1 +1,19 @@
-export class CreateMaquinaDto {}
+import mongoose from "mongoose";
+
+export class CreateMaquinaDto {
+    Nome: string;
+    Descricao: string;
+    Peso: number;
+    Comprimento: number;
+    Largura: number;
+    Altura: number;
+    Imagens: Array<string>;
+    EstaAtiva: boolean;
+    DataDeCriacao: Date;
+    Avaliacoes: mongoose.Schema.Types.ObjectId;
+    // Avaliacoes: string;
+    DonoDaMaquina: object;
+    Categoria: object;
+    Endereco: object;
+    Preco: object;
+}
--- apps/back/src/maquina/entities/maquina.entity.ts
+++ apps/back/src/maquina/entities/maquina.entity.ts
@@ -1,1 +1,77 @@
-export class Maquina {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+export type MaquinaDocument = HydratedDocument<Maquina>;
+
+@Schema()
+
+
+export class Tipo {
+    // @Prop({type: mongoose.Schema.Types.ObjectId, ref: 'TipoPreco'})
+    // _id: TipoPreco
+
+    // @
+
+}
+
+export class Preco {
+    @Prop()
+    _id: mongoose.Schema.Types.ObjectId
+
+    @Prop({type: Tipo})
+    Tipo: Tipo;
+}
+
+export class Maquina {
+  @Prop()
+  Nome: string;
+
+  @Prop()
+  Descricao: string;
+
+  @Prop()
+  Peso: number;
+
+  @Prop()
+  Comprimento: number;
+
+  @Prop()
+  Largura: number;
+
+  @Prop()
+  Altura: number;
+
+  @Prop()
+  Imagens: Array<string>;
+
+  @Prop()
+  EstaAtiva: boolean;
+
+  @Prop()
+  DataDeCriacao: Date;
+
+  // a fazer avaliacoes
+//   @Prop()
+//   Avaliacoes: Avaliacoes[]
+
+  @Prop()
+  DonoDaMaquina: object;
+
+  @Prop()
+  Categoria: object;
+
+  @Prop()
+  Endereco: object;
+
+  @Prop({type: Preco})
+  Preco: Preco;
+
+}
+
+
+
+
+
+
+
+export const MaquinaSchema = SchemaFactory.createForClass(Maquina);
