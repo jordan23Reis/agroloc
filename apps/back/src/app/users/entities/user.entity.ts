13bf1ec2428c849bac2ed1c58644e743912b0a38
MauuroCesarSantanadeJesus <eununcalembrodaminhaconta@gmail.com>
2023-08-30 22:29:25 -0300

    Aperfeiçoamento do crud usuario

--- apps/back/src/app/users/entities/user.entity.ts
+++ apps/back/src/app/users/entities/user.entity.ts
@@ -1,10 +1,10 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { Date, HydratedDocument } from 'mongoose';
 
 export type UsuarioDocument = HydratedDocument<Usuario>;
 
 @Schema()
-export class Categoria {
+class Categoria {
   //====================================
   //A IMPLEMENTAR
   //====================================
@@ -15,7 +15,8 @@ export class Categoria {
 }
 
 @Schema()
-export class Automovel {//estou exportando a class automovel para ser usada
+class Automovel {
+  //estou exportando a class automovel para ser usada
   @Prop()
   Nome: string;
   @Prop()
@@ -34,18 +35,133 @@ export class Automovel {//estou exportando a class automovel para ser usada
   Categoria: Categoria;
 }
 
+//depois que crio a classe e insiro os atributos eu preciso chamar a @Prop({ type: + nome da classe})
+// se for um array preciso coloca-lo dentro de [] ex: depois na linha posterior
+//chamar a classe novamente e atribuir a classe ex: @Prop({ type: [Automovel] })
+//CadastroComum: CadastroComum;
 @Schema()
-export class CadastroFreteiro {
+class CadastroFreteiro {
   @Prop()
   CNH: string;
-  @Prop({ type: Automovel })// aqui o type faz referência ao padrão do nest
-  Automovel: Automovel;
+  @Prop({ type: [Automovel] }) // aqui o type faz referência ao padrão do nest
+  Automovel: Automovel[];
 }
-
+@Schema()
+class Enderecos {
+  @Prop()
+  Cep: string;
+  @Prop()
+  Cidade: string;
+  @Prop()
+  Bairro: string;
+  @Prop()
+  Logradouro: string;
+  @Prop()
+  Complemento: string;
+  @Prop()
+  Numero: number;
+}
+@Schema()
+class CadastroComum {
+  @Prop()
+  NomeCompleto: string;
+  @Prop()
+  DataDeNascimento: Date;
+  @Prop()
+  Sexo: string;
+  @Prop()
+  Telefone: Array<string>;
+  @Prop()
+  Cpf: string;
+  @Prop()
+  Cnpj: string;
+  @Prop()
+  Foto: string;
+  @Prop({ type: [Enderecos] })
+  Enderecos: [Enderecos];
+}
+@Schema()
+class Pix {
+  @Prop()
+  Chave: string;
+  @Prop()
+  Tipo: string;
+}
+@Schema()
+class ContaBancaria {
+  @Prop()
+  Agencia: string;
+  @Prop()
+  Conta: string;
+}
+@Schema()
+class InformacoesBancarias {
+  @Prop({ type: ContaBancaria })
+  ContaBancaria: ContaBancaria;
+  @Prop({ type: Pix })
+  Pix: Pix;
+}
+@Schema()
+class Login {
+  @Prop()
+  UserName: string;
+  @Prop()
+  Email: string;
+  @Prop()
+  Senha: string;
+  @Prop()
+  Salt: string;
+  @Prop()
+  Tipo: string;
+}
 @Schema()
 export class Usuario {
+  @Prop({ type: CadastroComum })
+  CadastroComum: CadastroComum;
   @Prop({ type: CadastroFreteiro })
   CadastroFreteiro: CadastroFreteiro;
+  @Prop({ type: Login })
+  Login: Login;
+
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Maquina' }] })
+  // Maquinas: Maquina[];
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Favorito' }] })
+  // Favoritos: Favorito[];
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProcessoDeAluguel' }] })
+  // MaquinasAlugadas: ProcessoDeAluguel[];
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProcessoDeAluguel' }] })
+  // MaquinasLocadas: ProcessoDeAluguel[];
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProcessoDeFrete' }] })
+  // FretesRealizados: ProcessoDeFrete[];
+
+  //====================================
+  //A IMPLEMENTAR
+  //====================================
+  // @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProcessoDeFrete' }] })
+  // FretesSolicitados: ProcessoDeFrete[];
+  @Prop({ type: InformacoesBancarias })
+  InformacoesBancarias: InformacoesBancarias;
+
 }
 
 export const MaquinaSchema = SchemaFactory.createForClass(Usuario);
--- apps/back/src/app/users/users.service.ts
+++ apps/back/src/app/users/users.service.ts
@@ -1,26 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Users } from 'mongoose';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectModel(Users.name) private UserModel: Model<Users>,
+  ) {}
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return createUserDto;
   }
 
   findAll() {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
+  update(id: string, updateUserDto: UpdateUserDto) {
     return `This action updates a #${id} user`;
   }
 
-  remove(id: number) {
+  remove(id: string ){
     return `This action removes a #${id} user`;
   }
 }
