62c8635ee653020b38360082348af152a3f19fe0
pedro <pedro@gmail.com>
2023-09-06 22:08:29 -0300

    desenvolvendo user-dto

--- apps/back/src/users/dto/create-user.dto.ts
+++ apps/back/src/users/dto/create-user.dto.ts
@@ -1,19 +1,104 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from '@nestjs/class-validator';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+  ValidateNested,
+  isNotEmpty,
+} from '@nestjs/class-validator';
 import mongoose from 'mongoose';
+import { Type } from 'class-transformer'
 
-  class CadastroCompletoUsuario {
-    _id: mongoose.Schema.Types.ObjectId;
-    nome: string;
-    Senha: string;
-    Cpf: string;
-    Cnpj: string;
-  } 
+class CadastroComum {
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  NomeCompleto: string;
+  @IsNotEmpty()
+  DataDeNascimento: Date;
+  @IsNotEmpty()
+  Sexo: string;
+  @IsNotEmpty()
+  Telefone: string[];
+  @IsNotEmpty()
+  Cpf: string;
+  @IsNotEmpty()
+  Cnpj: string;
+  @IsOptional()
+  Foto: string;
+  @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => Enderecos)
+  Enderecos: Enderecos[];
+}
 
+class Enderecos {
+  //idEnderecos: mongoose.Schema.Types.ObjectId;
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  Cep: string;
+  @IsNotEmpty()
+  Cidade: string;
+  @IsNotEmpty()
+  Bairro: string;
+  @IsNotEmpty()
+  Logradouro: string;
+  @IsOptional()
+  Complemento: string;
+  @IsNotEmpty()
+  Numero: number;
+}
 
-  export class CreateUserDto {
+class Login {
+  @IsNotEmpty()
+  UserName: string;
+  @IsNotEmpty()
+  Email: string;
+  @IsNotEmpty()
+  Senha: string;
+  @IsNotEmpty()
+  Salt: string;
+  @IsNotEmpty()
+  Tipo: string;
+}
 
-CadastroCompleto: CadastroCompletoUsuario;
+class CadastroFreteiro {
+ CNH: string;
+ @IsNotEmpty()
+  @ValidateNested()
+  @Type(() => Automovel)
+  Automovel: Automovel[];
+}
 
-  }
+class Automovel {
+  //idAutomovel
+  _id: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  Nome: string;
+  @IsNotEmpty()
+  Descricao: string;
+  @IsNotEmpty()
+  Peso: number;
+  @IsNotEmpty()
+  Comprimento: number;
+  @IsNotEmpty()
+  Largura: number;
+  @IsNotEmpty()
+  Altura: number;
+  @IsNotEmpty()
+  Imagens: string[];
+  Categoria: Categoria[];
+}
 
+class Categoria {
+  idCategoria: mongoose.Schema.Types.ObjectId;
+  @IsNotEmpty()
+  Nome: string;
+}
+export class CreateUserDto {
 
+  CadastroComum: CadastroComum;
+  Login: Login;
+  CadastroFreteiro: CadastroFreteiro;
+}
