599ca770afbb1c9f4c42678941fabe7a359274f9
jordanbreis <114694316+jordanbreis@users.noreply.github.com>
2023-09-22 18:19:50 -0300

    base do MachineryRegister criado

--- libs/machinery/feature/src/lib/register/register.component.spec.ts
+++ libs/machinery/feature/src/lib/register/register.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RegisterComponent } from './register.component';
+import { MachineryRegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+  let component: MachineryRegisterComponent;
+  let fixture: ComponentFixture<MachineryRegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [RegisterComponent],
+      declarations: [MachineryRegisterComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(RegisterComponent);
+    fixture = TestBed.createComponent(MachineryRegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- libs/machinery/feature/src/lib/register/register.component.ts
+++ libs/machinery/feature/src/lib/register/register.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'agroloc-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class MachineryRegisterComponent {}
+export class MachineryRegisterComponent {
+  platform = inject(Platform);
+  isMobile = this.platform.ANDROID || this.platform.IOS;
+}
