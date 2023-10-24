9d457ecace7f420f557b2d1c71c56962dd549d2b
Lucas Mella <lucasmella16@gmail.com>
2023-10-24 02:55:54 -0300

    Entity finalizada

--- libs/shared/util/src/lib/schema-dto-constraints/processodealuguel.ts
+++ libs/shared/util/src/lib/schema-dto-constraints/processodealuguel.ts
@@ -6,7 +6,67 @@ enum ProcessoDeAluguelSchemaDtoRestraints {
     tamMaxDataTermino = 100000000000000000000000000000000000,
 
     tamMinStatus = 3,
-    tamMaxStatus = 100
+    tamMaxStatus = 100,
+
+    tamMinStatusPagamento=0,
+    tamMaxStatusPagamento=100,
+    
+    minQuantificadorPreco=0,
+    maxQuantificadorPreco=999999999999999,
+
+    minValorPagamento=0,
+    maxValorPagamento=999999999999999,
+
+    tamMinTipoPagamento=0,
+    tamMaxTipoPagamento=150,
+
+    tamMinContaPagamento=0,
+    tamMaxContaPagamento=200,
+
+    tamMinAgenciaPagamento=0,
+    tamMaxAgenciaPagamento=200,
+
+    tamMinTipoPix=0,
+    tamMaxTipoPix=200,
+
+    tamMinChavePix=0,
+    tamMaxChavePix=200,
+
+    minValorPorTipoPreco=0,
+    maxValorPorTipoPreco=999999999999999,
+
+    tamMinNomeMaquina=3,
+    tamMaxNomeMaquina=100,
+
+    tamMinIdMaquina=0,
+    tamMaxIdMaquina=150,
+
+    tamMinNomeLocatario=0,
+    tamMaxNomeLocatario=200,
+
+    tamMinIdLocatario=0,
+    tamMaxIdLocatario=150,
+    
+    tamMinNomeLocador=0,
+    tamMaxNomeLocador=200,
+
+    tamMinIdLocador=0,
+    tamMaxIdLocador=150,
+
+    tamMinNomeArquivoImagem=1,
+    tamMaxNomeArquivoImagem=1000,
+
+    tamMinUrlImagem=1,
+    tamMaxUrlImagem=1000,
+
+    tamMinNomeTipo=3,
+    tamMaxNomeTipo=150,
+
+    tamMinIdTipo=0,
+    tamMaxIdTipo=150
+
+
+
 }
 
 export {ProcessoDeAluguelSchemaDtoRestraints};
