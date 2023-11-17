e7a0c4d0554acaf40ca8b59e7a6f4d308b852116
Jordan Borges Dos Reis <jordan.reis@alunos.unicesumar.edu.br>
2023-11-16 22:25:35 -0300

    tela do solicitação de aluguel

--- libs/rent/feature/src/lib/rent-feature.module.ts
+++ libs/rent/feature/src/lib/rent-feature.module.ts
@@ -6,6 +6,8 @@ import { NegotiateComponent } from './negotiate/negotiate.component';
 import { MatIconModule } from '@angular/material/icon';
 import { ImageModule } from 'primeng/image';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
@@ -15,6 +17,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ImageModule,
     FormsModule,
     ReactiveFormsModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   declarations: [NegotiateComponent],
   exports: [NegotiateComponent],
